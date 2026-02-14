5eca4ec08c2932f0018e28b09af970b1a1b534ad
Josef Karasek <karasek.jose@gmail.com>
2026-02-14 13:54:12 +0100

    a
    Signed-off-by: Josef Karasek <karasek.jose@gmail.com>

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,6 +3,12 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.vuejs.org/config/app-configs
 
 export default defineConfig({
+  title: 'Autopunk 2026',
+  description: 'Off-road itinerář: Ázerbájdžán',
   base: '/autopunk-2026/',
-  // ...
+  themeConfig: {
+    nav: [
+      { text: 'Itinerář', link: '/' }
+    ]
+  }
 })
